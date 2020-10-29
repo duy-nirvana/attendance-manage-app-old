7fc58a54353091159aefc78ff34611690ee0a9a7
duy-nirvana <trankhanhduy8599@gmail.com>
2020-10-29 21:36:44 +0700

    Change background-color navigation

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import ScanScreen from './src/screens/scan';
 import SettingsScreen from './src/screens/settings';
@@ -12,22 +12,29 @@ const Tab = createMaterialBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#fff" translucent = {true}/>
       <Tab.Navigator
         initialRouteName="Feed"
-        activeColor="#fff"
-        style={{ backgroundColor: 'tomato' }}
-      >
+        activeColor= "navy"
+        barStyle={{ backgroundColor: '#fff' }}
+        >
         <Tab.Screen 
           name="Scan"
           component={ScanScreen}
           options={{
           tabBarLabel: 'Scan',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="qrcode" color={color} size={26} />
+          ),
         }}/>
         <Tab.Screen 
           name="Settings"
           component={SettingsScreen}
           options={{
           tabBarLabel: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="settings" color={color} size={26} />
+          ),
         }}/>
       </Tab.Navigator>
     </NavigationContainer>
