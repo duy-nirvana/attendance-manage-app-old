307e12e17c61f1aa744137cf7dc8bf6dd666249d
duy-nirvana <trankhanhduy8599@gmail.com>
2020-10-30 19:57:18 +0700

    Implement UI for settingsScreen

--- src/screens/settings/index.js
+++ src/screens/settings/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, View, Dimensions, StatusBar} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
+
+const width = Dimensions.get('window').width; //full width
+const statusBarHeight = StatusBar.currentHeight;
 
 const SettingsScreen = (props) => {
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings</Text>
+        <View style={{flex: 1, alignItems: 'center', paddingTop: statusBarHeight + 20, backgroundColor: 'white'}}>
+            <Image 
+                style={{width: 100, height: 100, borderRadius: 40/2, marginBottom: 20}}
+                source={{
+                    uri: "https://www.shareicon.net/data/2015/09/20/104335_avatar_512x512.png"
+                }}
+            />
+            <TextInput
+                style={{width: width * .9,  backgroundColor: 'white'}}
+                label="HỌ VÀ TÊN"
+                value="Khánh Duy"
+                editable={false}
+            />
+            <TextInput
+                style={{width: width * .9,  backgroundColor: 'white'}}
+                label="MSSV"
+                value="1711062181"
+                editable={false}
+            />
+            <TextInput
+                style={{width: width * .9,  backgroundColor: 'white'}}
+                label="LỚP"
+                value="17DTHC5"
+                editable={false}
+            />
+            <TextInput
+                style={{width: width * .9,  backgroundColor: 'white'}}
+                label="EMAIL"
+                value="duy@gmail.com"
+                editable={false}
+            />
+            <Button mode="outlined" color="white" style={{width: width * .9,  backgroundColor: 'navy', marginTop: 20}}> 
+                ĐỔI MẬT KHẨU
+            </Button>
+            <Button mode="outlined" color="white" style={{width: width * .9,  backgroundColor: 'black', marginTop: 20}}> 
+                ĐĂNG XUẤT
+            </Button>
         </View>
     )
 }
